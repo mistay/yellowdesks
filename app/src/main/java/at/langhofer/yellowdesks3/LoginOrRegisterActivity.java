d86035ca6cdd738979d6989eec733886bbb2f9e8
Armin Langhofer <arminlanghofer@Armins-MacBook-Pro.local>
2016-11-18 23:41:54 +0100

    added activity: map

--- app/src/main/java/at/langhofer/yellowdesks3/LoginOrRegisterActivity.java
+++ app/src/main/java/at/langhofer/yellowdesks3/LoginOrRegisterActivity.java
@@ -1,8 +1,12 @@
 package at.langhofer.yellowdesks3;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.WindowManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import static android.R.attr.value;
 
 public class LoginOrRegisterActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class LoginOrRegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_or_register);
+
+        final Button btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(LoginOrRegisterActivity.this, MapActivity.class);
+                myIntent.putExtra("key", value); //Optional parameters
+                LoginOrRegisterActivity.this.startActivity(myIntent);
+            }
+        });
     }
 }
