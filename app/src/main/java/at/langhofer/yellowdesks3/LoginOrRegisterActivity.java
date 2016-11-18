7278dec1056ac7e568ed0ee32ee376deb0337fc9
Armin Langhofer <arminlanghofer@Armins-MacBook-Pro.local>
2016-11-18 23:24:21 +0100

    yellowdesks: init commit

---
+++ app/src/main/java/at/langhofer/yellowdesks3/LoginOrRegisterActivity.java
@@ -0,0 +1,14 @@
+package at.langhofer.yellowdesks3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class LoginOrRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_or_register);
+    }
+}
