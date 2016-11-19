306c6984876c891a2741e1ba98b97c769b2ac804
Armin Langhofer <arminlanghofer@Armins-MacBook-Pro.local>
2016-11-19 13:19:28 +0100

    map: added header buttons: opennow, on date and list

--- app/src/main/java/at/langhofer/yellowdesks3/MapActivity.java
+++ app/src/main/java/at/langhofer/yellowdesks3/MapActivity.java
@@ -1,8 +1,9 @@
 package at.langhofer.yellowdesks3;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -10,19 +11,11 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import static at.langhofer.yellowdesks3.R.id.map;
+
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-    }
 
 
     /**
@@ -38,9 +31,48 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng sydney = new LatLng(47.82283700000001, 13.04061200000001);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Romy"));
+
+        LatLng tmp = new LatLng(47.7905403, 13.07501769999999);
+        mMap.addMarker(new MarkerOptions().position(tmp).title("Mike"));
+
+        tmp = new LatLng(47.8055588, 13.047860000000014);
+        mMap.addMarker(new MarkerOptions().position(tmp).title("Thomas"));
+
+        tmp = new LatLng(47.8223683, 13.071454000000017);
+        mMap.addMarker(new MarkerOptions().position(tmp).title("Heidi"));
+
+        tmp = new LatLng(47.782805, 13.064100999999937);
+        mMap.addMarker(new MarkerOptions().position(tmp).title("Armin"));
+
+        tmp = new LatLng(47.855221, 13.093958799999996);
+        mMap.addMarker(new MarkerOptions().position(tmp).title("Christian"));
+
+        tmp = new LatLng(47.806021, 13.050602000000026);
+        mMap.addMarker(new MarkerOptions().position(tmp).title("Max"));
+
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setAllGesturesEnabled(true);
+        mMap.getUiSettings().setZoomGesturesEnabled(true);
+
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(sydney, 12);
+        mMap.moveCamera(update);
     }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(map);
+        mapFragment.getMapAsync(this);
+
+
+
+    }
+
+
+
 }
