52ea9b738f4c94380aa54bd84311f562f006df97
armin <office@langhofer.at>
2017-02-13 17:11:34 +0100

    properly resolving clicked item to host

--- app/src/main/java/at/langhofer/yellowdesks/YellowdeskRowAdapter.java
+++ app/src/main/java/at/langhofer/yellowdesks/YellowdeskRowAdapter.java
@@ -3,6 +3,7 @@ package at.langhofer.yellowdesks;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,11 +11,8 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.graphics.drawable.BitmapDrawable;
 
 import java.util.List;
-
-import at.langhofer.yellowdesks.R;
 
 import static java.lang.String.format;
 
@@ -34,6 +32,9 @@ public class YellowdeskRowAdapter extends ArrayAdapter<Host> {
 
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
+
+        System.out.println("getView()");
+
         View view = convertView;
         final ViewHolder holder;
 
@@ -56,25 +57,39 @@ public class YellowdeskRowAdapter extends ArrayAdapter<Host> {
         holder.deskstatus = (TextView) view.findViewById(R.id.deskstatus);
         holder.imgView = (ImageView) view.findViewById(R.id.resultimage);
 
-        holder.deskstatus.setText(format("YELLOW desks: %d/%d", host.gettotalDesks(), host.getAvailableDesks()));
+        holder.deskstatus.setText(format("%s: Yellow Desks: %d/%d", host.getHost(), host.gettotalDesks(), host.getAvailableDesks()));
 
         Drawable myDrawable = null;
 
         Bitmap bitmap = host.getBitmap();
 
+
+
+
         if (bitmap != null) {
             holder.imgView.setImageDrawable(new BitmapDrawable(bitmap));
         } else {
+
+            // muss man machen, sonst werden die images mit gecachten bildern geladen.
+            holder.imgView.setImageDrawable(null);
+
             DelegateImageDownloaded downloadFinished = new DelegateImageDownloaded() {
                 @Override
                 public void imageDownloaded(Bitmap result) {
-                    System.out.println("imageDownloaded, result: " + result.toString());
+                    if (result != null) {
+                        System.out.println("imageDownloaded, result: " + result.toString());
 
-                    Drawable myDrawable = new BitmapDrawable(result);
-                    holder.imgView.setImageDrawable(myDrawable);
+                        Drawable myDrawable = new BitmapDrawable(result);
+                        holder.imgView.setImageDrawable(myDrawable);
+                    } else {
+                        System.out.println("DelegateImageDownloaded downloadFinished but result was null :(");
+                    }
                 }
             };
-            Data.getInstance().downloadImage(host, downloadFinished);
+
+            System.out.println("downloadImage. id: " + host.getId() + " url: " +  host.getImageURL() + " isnull? " + ( host.getImageURL() == null));
+            if (host.getImageURL() != null)
+                Data.getInstance().downloadImage(host, downloadFinished);
         }
 /*
         myDrawable = view.getContext().getResources().getDrawable(R.drawable.alex);
