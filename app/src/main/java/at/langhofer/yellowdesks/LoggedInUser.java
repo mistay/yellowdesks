7a6bdac8ccaed95bce0480177f4bded7671cc3e9
armin langhofer <armin@langhofer.net>
2017-03-03 00:48:38 +0100

    LoggedInUser.java: removed email, realname and facebook_id, now handled in logindetails

--- app/src/main/java/at/langhofer/yellowdesks/LoggedInUser.java
+++ app/src/main/java/at/langhofer/yellowdesks/LoggedInUser.java
@@ -1,13 +1,7 @@
 package at.langhofer.yellowdesks;
 
 public class LoggedInUser {
-    public static String email = "";
-    public static String realname = "";
-    public static String facebook_id = null;
-
     public final static String PREFLOGINTARGET = "PREFLOGINTARGET";
     public final static String PREFUSERNAME = "PREFUSERNAME";
     public final static String PREFPASSWORD = "PREFPASSWORD";
-
-
 }
