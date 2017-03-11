61a6224f2f88bcf65a7bbbb0586e6121fe3345fc
armin langhofer <armin@langhofer.net>
2017-03-11 22:03:06 +0100

    Developerdevices.java: init commit

---
+++ app/src/main/java/at/langhofer/yellowdesks/Developerdevices.java
@@ -0,0 +1,18 @@
+package at.langhofer.yellowdesks;
+
+import android.os.Build;
+
+public class Developerdevices {
+    public static String[] devices = {
+            "043059138291dd6f" /*armin*/,
+
+    };
+
+    public static Boolean isDeveloperDevice() {
+        for (String s : Developerdevices.devices)
+            if (Build.SERIAL.equals( s ))
+                return true;
+
+        return false;
+    }
+}
